7181b6d97d081d58d725534a0bfeb55cb2766c2a
nick <nick.m.dodson@gmail.com>
2024-07-17 19:36:26 -0400

    edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 lg:p-24">
 
       <div className="flex flex-col gap-4 w-3/4">
-        <h1 className="text-3xl">Welcome to Calabara</h1>
-        <p>
-          Since our founding back in 2021, we have been committed to building public goods for the web3 universe and providing our users
-          with efficient ways to co-create masterpieces with their respective communities. 
-        </p>
+        <div className="flex flex-col">
+          <h1 className="text-3xl">Calabara</h1>
+          <h2 className="text-lg italic">/kuh,lah,buh,ruh/</h2>
+        </div>
+        <h2 className="text-xl">
+          Founded in 2021, we build public goods for onchain creators and communities.
+        </h2>
+        <div className="flex flex-col gap-2 text-xl">
+          <h2 className="">We are the founders & maintainers of</h2>
+          <Link href="https://uplink.wtf/" className="flex gap-2">
+            <Image
+              src="/uplink-logo.svg"
+              width={15}
+              height={15}
+              alt="pfp"
+              className=""
+            />
+
+            <p className="font-bold text-[#cc0595] hover:underline">Uplink</p>
+          </Link>
+          <h2 className=""> and creators of the</h2>
+          <Link href="https://github.com/calabara-hq/transmissions" target="_blank" className=" text-[#cc0595] font-bold hover:underline">Transmissions Protocol</Link>
+        </div>
         <hr className="border-t border-gray-300 my-4" />
 
         <h1 className="text-3xl">
@@ -19,10 +38,10 @@ export default function Home() {
           <div className="flex gap-4 ">
             <Image
               src="/nick-pfp.png"
-              width={90}
-              height={90}
+              width={50}
+              height={50}
               alt="pfp"
-              className="rounded-full w-20 sm:w-16 md:w-20 lg:w-24"
+              className="w-20 "
             />
             <div className="flex flex-col gap-1">
               <p className="text-xl font-bold">
@@ -39,10 +58,10 @@ export default function Home() {
           <div className="flex gap-4 ">
             <Image
               src="/josh-pfp.png"
-              width={90}
-              height={90}
+              width={50}
+              height={50}
               alt="pfp"
-              className="rounded-full w-20 sm:w-16 md:w-20 lg:w-24"
+              className="w-20 "
             />
             <div className="flex flex-col gap-1">
               <p className="text-xl font-bold">
@@ -58,27 +77,9 @@ export default function Home() {
           </div>
         </div>
         <hr className="border-t border-gray-300 my-4" />
-
-        <div className="flex flex-col gap-4">
-          <h2 className="text-3xl">We are the founders of:</h2>
-          <div className="flex gap-2">
-            <Image
-              src="/uplink-logo.svg"
-              width={18}
-              height={18}
-              alt="pfp"
-              className=""
-            />
-
-            <a href="https://uplink.wtf/" target="_blank" className="text-xl font-bold hover:underline">Uplink</a>
-          </div>
-          <p>a front end for onchain group creation and publication</p>
-          <p className="text-xl">&</p>
-          <a href="https://github.com/calabara-hq/transmissions" target="_blank" className="text-xl font-bold hover:underline">Transmission Protocol</a>
-        </div>
-      </div>
+      </div >
 
 
-    </main>
+    </main >
   );
 }
